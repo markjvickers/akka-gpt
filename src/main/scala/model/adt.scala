ef80c5f2572e38ad55d615b4ce4268bf706acf53
Mark Vickers <markjvickers@hotmail.com>
2023-04-02 21:53:07 -0400

    removed space

--- src/main/scala/model/adt.scala
+++ src/main/scala/model/adt.scala
@@ -9,4 +9,3 @@ case class GPT4Choice(text: String, index: Int, logprobs: Option[GPT4LogProbs], 
 case class GPT4LogProbs(tokens: List[String], tokenLogprobs: List[Double], topLogprobs: List[List[(String, Double)]], textOffset: List[Int])
 
 case class GPT4Error(code: String, message: String)
-
