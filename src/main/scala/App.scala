519f5a503fcbf162ca5acbfe2d737f529c238dfc
Mark Vickers <markjvickers@hotmail.com>
2023-04-23 21:25:42 -0400

    Update App.scala
    another test

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,6 +2,6 @@ object App {
   def greeting(msg: String): Unit = println(msg)
 
   def main(args: Array[String]): Unit = {
-    greeting(sample.Sample.test())
+    greeting(sample.Sample.test() + "bing")
   }
 }
